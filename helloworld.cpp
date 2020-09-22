59323b79320f5bc169e5f20b6d19f6331966cf05
Prince Krampah <jsksprince@gmail.com>
2020-09-22 10:44:29 +0300

    Day 01

---
+++ helloworld.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/*
+  code on line no.1 is used to include input and output 
+  functionality (input and ouput stream)
+*/
+
+using namespace std;
+
+/*
+	code on line no.8 allows us to use a name space, we can do without it
+	by prefixing cout and cin with std::cout << "Hello world" or std::cin >> name;
+
+
+	uncomment the code on line 21 to see this, simply remove the // in front of
+	the code. Then comment the code on line no.8 to see the effect.
+*/
+
+/*
+	Semi colon is used at the end of every line, without it you get an error.
+*/
+
+int main(){
+	cout <<  "Hello world" << endl;
+	// std::cout << "Hello world"
+
+	return 0;
+}
