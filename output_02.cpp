4a0491ccbd28fc488694ed8fa94c5a81029deded
Prince Krampah <jsksprince@gmail.com>
2020-09-28 13:54:06 +0300

    Day 02-output

---
+++ output_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Strathmore University Vision\n";
+	cout << "...............................\n";
+	cout << "leading out- come driven entrepreneurial\n"; 
+	cout << "research University by translating our\n"; 
+	cout << "excellence into major contribution to culture,\n";
+	cout << "economic well-being and quality of life.\n";
+	
+
+	return 0;
+}
+
+
---
+++ output_03.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Strathmore University Vision\n";
+	cout << "...............................\n"
+	 		<< "leading out- come driven entrepreneurial\n" 
+			<< "research University by translating our\n" 
+			<< "excellence into major contribution to culture,\n"
+			<< "economic well-being and quality of life.\n";
+	
+
+	return 0;
+}
+
+
